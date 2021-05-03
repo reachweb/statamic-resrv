389b4b7b14d6e5fefeba10509df70a474dd50b20
Iosif Chatzimichail <afonic@gmail.com>
2021-05-03 18:58:35 +0300

    Add locations method to Resrv tag

--- src/Tags/Resrv.php
+++ src/Tags/Resrv.php
@@ -4,38 +4,13 @@ namespace Reach\StatamicResrv\Tags;
 
 use Statamic\Tags\Tags;
 use Statamic\Facades\Collection;
-use Reach\StatamicResrv\Models\Availability;
+use Reach\StatamicResrv\Models\Location;
 
 class Resrv extends Tags
 {
-    public function collection()
+    public function locations()
     {
-        $collection = $this->params->get('from');
-        $order = $this->params->get('order', 'order');
-        $entries = $this->getEntries($collection, $order);
-        $entries = $this->addAvailability($entries);
-        return json_encode($entries->toAugmentedArray());
-    }
-
-    protected function getEntries($collection, $order)
-    {
-        return Collection::find($collection)
-            ->queryEntries()
-            ->where('published', true)
-            ->orderBy($order, 'asc')
-            ->get();
-    }
-
-    protected function addAvailability($entries)
-    {
-        $entries->each(function ($entry, $key) {
-            $availability_data = Availability::entry($entry->id())->get();
-            $data = $availability_data->sortBy('date')->keyBy('date')->toArray();
-            $cheapest = $availability_data->sortBy('price')->firstWhere('available', '>', '0');
-            $entry->set('availability', $data);
-            $entry->set('cheapest', $cheapest->price);
-        });
-        return $entries;
+        return htmlspecialchars(Location::all()->toJson(), ENT_QUOTES, 'UTF-8');
     }
 
 }
