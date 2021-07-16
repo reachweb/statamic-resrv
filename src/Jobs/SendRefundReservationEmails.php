1f2de73adb2e3c2dd5d6baf76104ef4b24742fb8
Iosif Chatzimichail <afonic@gmail.com>
2021-07-16 17:06:11 +0300

    Send refund emails to admin as well

--- src/Jobs/SendRefundReservationEmails.php
+++ src/Jobs/SendRefundReservationEmails.php
@@ -32,6 +32,13 @@ class SendRefundReservationEmails implements ShouldQueue
     public function handle()
     {   
         // Customer email
-        Mail::to($this->reservation->customer->get('email'))->send(new ReservationRefunded($this->reservation));        
+        Mail::to($this->reservation->customer->get('email'))->send(new ReservationRefunded($this->reservation));
+        // Admin emails if set
+        if (config('resrv-config.admin_email') != false) {
+            $admin_emails = explode(',', config('resrv-config.admin_email'));
+            foreach ($admin_emails as $email) {
+                Mail::to($email)->send(new ReservationRefunded($this->reservation));
+            }            
+        }    
     }
 }
