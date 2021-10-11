63837b6c751bc501a2b77d6fae55edf13ba96e54
Iosif Chatzimichail <afonic@gmail.com>
2021-10-11 16:54:51 +0300

    Wrong type in Dynamic pricing table

---
+++ database/migrations/2021_10_11_135143_change_dynamic_pricing_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeDynamicPricingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('resrv_dynamic_pricing', function (Blueprint $table) {
+            $table->string('date_include')->change();           
+        });
+    }
+
+}
