50e6e20d441c734158f9e0b20ac5cf837bfca18f
Iosif Chatzimichail <afonic@gmail.com>
2021-10-12 23:35:58 +0300

    Not needed

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,9 +22,7 @@ class TestCase extends OrchestraTestCase
     use PreventSavingStacheItemsToDisk;
 
     protected function setUp(): void
-    {
-        require_once __DIR__.'/ConsoleKernel.php';
-        
+    {       
         parent::setUp();
 
         $this->preventSavingStacheItemsToDisk();
