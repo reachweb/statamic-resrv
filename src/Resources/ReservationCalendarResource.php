0138f12f072f879b63b80fc1af8bf035581f24a2
Iosif Chatzimichail <afonic@users.noreply.github.com>
2022-04-07 01:27:50 +0300

    Fix calendar display for multi reservation

--- src/Resources/ReservationCalendarResource.php
+++ src/Resources/ReservationCalendarResource.php
@@ -13,13 +13,18 @@ class ReservationCalendarResource extends ResourceCollection
     }
 
     public function toArray($request)
-    {
-        return $this->collection->transform(function ($reservation) use ($request) {
+    {   
+        $childs = collect();
+        $reservations = $this->collection->transform(function ($reservation) use ($request, &$childs) {
+            if ($reservation->type === 'parent') {
+                $childs->push($this->buildChildReservationArray($reservation, $request)->toArray());
+                return false;
+            }
             $data = [
                 'id' => $reservation->id,
                 'title' => '#'.$reservation->id.
                             ' - '.$reservation->entry['title'].
-                            (config('resrv-config.enable_advanced_availability') ? ' - '.$reservation->property : '').
+                            (config('resrv-config.enable_advanced_availability') ? ' - '.$reservation->getPropertyAttributeLabel() : '').
                             (config('resrv-config.enable_locations') ? ' - '.$reservation->location_start_data->name : '').
                             (config('resrv-config.maximum_quantity') > 1 ? ' x '.$reservation->quantity : ''),
                 'start' => $this->formatDate($reservation->date_start),
@@ -35,7 +40,10 @@ class ReservationCalendarResource extends ResourceCollection
             }
 
             return $data;
-        });
+        })->reject(fn ($item) => $item === false);
+        $childs = $childs->flatten(1);
+        
+        return $reservations->concat($childs);
     }
 
     private function formatDate(?Carbon $date)
@@ -49,5 +57,30 @@ class ReservationCalendarResource extends ResourceCollection
         }
 
         return $date->toIso8601String();
+    }
+
+    protected function buildChildReservationArray($reservation, $request)
+    {
+        return $reservation->childs->map(function ($child) use ($reservation, $request) {
+            $data = [
+                'id' => $reservation->id,
+                'title' => '#'.$reservation->id.
+                            ' - '.$reservation->entry['title'].
+                            (config('resrv-config.enable_advanced_availability') ? ' - '.$child->getPropertyAttributeLabel() : '').
+                            (config('resrv-config.enable_locations') ? ' - '.$reservation->location_start_data->name : '').
+                            (config('resrv-config.maximum_quantity') > 1 ? ' x '.$child->quantity : ''),
+                'start' => $this->formatDate($child->date_start),
+                'end' => $this->formatDate($child->date_end),
+                'url' => cp_route('resrv.reservation.show', $reservation->id),
+                'color' => 'hsl('.rand(0, 359).','.rand(0, 100).'%,'.rand(0, 55).'%)',
+            ];
+            // Remove end date if we only want the start date
+            if ($request->has('onlyStart')) {
+                if ($request->query('onlyStart') == 1) {
+                    $data['end'] = null;
+                }
+            }
+            return $data;
+        });        
     }
 }
