d6bfe7b31f645158dfdc0dc711cdb6cd3f9d1f06
Iosif Chatzimichail <afonic@users.noreply.github.com>
2023-06-14 00:19:51 +0300

    Alternative take to maintain Laravel 9 support

--- src/Mail/Mailable.php
+++ src/Mail/Mailable.php
@@ -8,15 +8,41 @@ use Illuminate\Support\Arr;
 class Mailable extends LaravelMailable
 {
     // Override buildMarkdownView() to define new components path.
+    // TODO: Remove the legacy code when we drop support for Laravel 9.
     protected function buildMarkdownView()
     {
-        $this->markdownRenderer()->loadComponentsFrom(
-            file_exists(resource_path().'/views/vendor/statamic-resrv/email/theme')
-                ? [resource_path().'/views/vendor/statamic-resrv/email/theme']
-                : [__DIR__.'/../../resources/views/email/theme']
-        );
+        // Check if the markdownRenderer() method exists (Laravel 10 and later)
+        if (method_exists($this, 'markdownRenderer')) {
+            $this->markdownRenderer()->loadComponentsFrom(
+                file_exists(resource_path().'/views/vendor/statamic-resrv/email/theme')
+                    ? [resource_path().'/views/vendor/statamic-resrv/email/theme']
+                    : [__DIR__.'/../../resources/views/email/theme']
+            );
+            return parent::buildMarkdownView();
+        } 
+        // If markdownRenderer() doesn't exist (Laravel versions before 10)
+        else {
+            /** @var Markdown $markdown */
+            $markdown = Container::getInstance()->make(Markdown::class);
 
-        return parent::buildMarkdownView();
+            // Use package resources path
+            if (file_exists(resource_path().'/views/vendor/statamic-resrv/email/theme')) {
+                $markdown->loadComponentsFrom([
+                    resource_path().'/views/vendor/statamic-resrv/email/theme',
+                ]);
+            } else {
+                $markdown->loadComponentsFrom([
+                    __DIR__.'/../../resources/views/email/theme',
+                ]);
+            }
+
+            $data = $this->buildViewData();
+
+            return [
+                'html' => $markdown->render($this->markdown, $data),
+                'text' => $this->buildMarkdownText($markdown, $data),
+            ];
+        }
     }
 
     protected function getOption($option, $offset = 0)
