3a57c259253687e1b0a94de1edb9f7fe7b9cad6b
Iosif Chatzimichail <afonic@users.noreply.github.com>
2024-03-22 12:22:26 +0200

    Prevent going directly to step 2

--- resources/views/livewire/components/checkout-steps.blade.php
+++ resources/views/livewire/components/checkout-steps.blade.php
@@ -25,11 +25,11 @@
     </li>
     @endif
     <li 
-        wire:click="{{ $step !== 2 ? 'goToStep(2)' : '' }}" 
+        wire:click="{{ $step > 2 ? 'goToStep(2)' : '' }}" 
         @class([
             "flex md:w-full items-center sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 transition-colors duration-300", 
             "text-blue-600" => $step === 2,
-            "cursor-pointer" => $step !== 2,
+            "cursor-pointer" => $step > 2,
             ])
     >
         <span class="flex basis-full items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200">
