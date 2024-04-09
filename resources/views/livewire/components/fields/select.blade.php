1000208d2a3bfd178efbd5bb5ea201024022a212
Iosif Chatzimichail <afonic@users.noreply.github.com>
2024-04-10 01:28:08 +0300

    Small typo

--- resources/views/livewire/components/fields/select.blade.php
+++ resources/views/livewire/components/fields/select.blade.php
@@ -6,7 +6,6 @@
     </label>
     <select
         wire:model="form.{{ $field['handle'] }}"
-        type="text" 
         id="{{ $field['handle'] }}" 
         class="form-select bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         @if (array_key_exists('instructions', $field))
