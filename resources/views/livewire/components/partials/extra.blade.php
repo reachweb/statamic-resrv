43cda1bcd2abbc4b4ca0618f0511b6aa8b805bb4
Iosif Chatzimichail <afonic@users.noreply.github.com>
2025-04-09 14:59:29 +0300

    Better show / hide handling

--- resources/views/livewire/components/partials/extra.blade.php
+++ resources/views/livewire/components/partials/extra.blade.php
@@ -1,25 +1,21 @@
 @php
     $compact = $isCompact ?? false;
 @endphp
-@unless ($this->hiddenExtras->contains($extra->id))
+
 <div 
     wire:key="extra-{{ $extra->id }}"
-    x-transition:enter="transition ease-out duration-300"
-    x-transition:enter-start="opacity-0 -translate-y-2"
-    x-transition:enter-end="opacity-100 translate-y-0"
-    x-transition:leave="transition ease-in duration-100"
-    x-transition:leave-start="opacity-100 translate-y-0"
-    x-transition:leave-end="opacity-0 translate-y-2"
+    wire:loading.class="opacity-50 pointer-events-none"
     @class([
-        'grid grid-cols-4 items-center',
+        'grid grid-cols-4 items-center transition-opacity duration-150',
         'my-3 lg:my-5' => ! $compact, 
-        'my-2 lg:my-3' => $compact
+        'my-2 lg:my-3' => $compact,
+        'hidden' => $this->hiddenExtras->contains($extra->id)
     ])
 >
     <div @class([
         'order-0', 
         'col-span-3' => ! $extra->allow_multiple || ($extra->allow_multiple && $compact), 
-        'col-span-2' => $extra->allow_multiple && ! $compact
+        'col-span-2' => $extra->allow_multiple && ! $compact,
     ])>
         <div>
             <label class="inline-flex items-center cursor-pointer">
@@ -31,11 +27,11 @@
                     {{ $this->isExtraSelected($extra->id) ? 'checked' : '' }}>
                     <div 
                         class="relative flex-shrink-0 w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 
-                            peer-focus:ring-blue-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full 
-                            peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] 
-                            after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
-                            after:w-5 after:h-5 after:transition-all peer-checked:bg-blue-600
-                            {{ $this->requiredExtras->contains($extra->id) ? 'cursor-not-allowed' : '' }}"
+                        peer-focus:ring-blue-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full 
+                        peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] 
+                        after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
+                        after:w-5 after:h-5 after:transition-all peer-checked:bg-blue-600
+                        {{ $this->requiredExtras->contains($extra->id) ? 'cursor-not-allowed' : '' }}"
                     >
                 </div>
                 <span class="ms-3 font-medium text-gray-900">{{ $extra->name }}</span>
@@ -59,4 +55,3 @@
         <span>{{ config('resrv-config.currency_symbol') }} {{ $extra->price }}</span>
     </div>
 </div>
-@endunless
