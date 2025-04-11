d3953cdea112a69c502131739651e628e9e87a73
Iosif Chatzimichail <afonic@users.noreply.github.com>
2025-04-11 11:21:43 +0300

    Not needed

--- resources/views/livewire/components/partials/extra.blade.php
+++ resources/views/livewire/components/partials/extra.blade.php
@@ -20,7 +20,7 @@
                     {{ $this->isExtraSelected($extra->id) ? 'checked' : '' }}>
                     <div 
                         class="relative flex-shrink-0 w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 
-                        peer-focus:ring-blue-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full 
+                        peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full 
                         peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] 
                         after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
                         after:w-5 after:h-5 after:transition-all peer-checked:bg-blue-600
