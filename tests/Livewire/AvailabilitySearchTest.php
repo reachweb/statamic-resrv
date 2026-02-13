5677691daa6a153b53208843d2c9841987313b4d
Iosif Chatzimichail <afonic@users.noreply.github.com>
2026-02-13 13:33:42 +0200

    Calendar view fix

--- tests/Livewire/AvailabilitySearchTest.php
+++ tests/Livewire/AvailabilitySearchTest.php
@@ -381,7 +381,7 @@ class AvailabilitySearchTest extends TestCase
 
         $calendar = $component->effects['returns'][0];
 
-        $key = $this->date->format('Y-m-d').' 00:00:00';
+        $key = $this->date->format('Y-m-d');
 
         $this->assertIsArray($calendar);
         $this->assertArrayHasKey($key, $calendar);
@@ -406,7 +406,7 @@ class AvailabilitySearchTest extends TestCase
 
         $calendar = $component->effects['returns'][0];
 
-        $key = $this->date->format('Y-m-d').' 00:00:00';
+        $key = $this->date->format('Y-m-d');
 
         $this->assertIsArray($calendar);
         $this->assertArrayHasKey($key, $calendar);
@@ -482,7 +482,7 @@ class AvailabilitySearchTest extends TestCase
             ->call('availabilityCalendar');
 
         $calendar = $component->effects['returns'][0];
-        $key = today()->format('Y-m-d').' 00:00:00';
+        $key = today()->format('Y-m-d');
 
         $this->assertIsArray($calendar);
         $this->assertArrayHasKey($key, $calendar);
@@ -490,5 +490,22 @@ class AvailabilitySearchTest extends TestCase
         // Note: The entry already has availability at price 50, so 25 should be returned
         $this->assertEquals(25, $calendar[$key]['price']);
         $this->assertEquals('cheap-cabin', $calendar[$key]['property']);
+    }
+
+    public function test_availability_calendar_keys_are_date_only_format()
+    {
+        $component = Livewire::test(AvailabilitySearch::class, [
+            'entry' => $this->entries->first()->id(),
+            'showAvailabilityOnCalendar' => true,
+        ])
+            ->call('availabilityCalendar');
+
+        $calendar = $component->effects['returns'][0];
+
+        $this->assertNotEmpty($calendar);
+
+        foreach (array_keys($calendar) as $key) {
+            $this->assertMatchesRegularExpression('/^\d{4}-\d{2}-\d{2}$/', $key, "Calendar key '{$key}' should be in Y-m-d format without time component");
+        }
     }
 }
