adb827ce32fd56c03c400131f2153524c95ebd04
Iosif Chatzimichail <afonic@users.noreply.github.com>
2026-05-18 21:39:28 +0300

    Shortcut to settings

--- src/Providers/ResrvProvider.php
+++ src/Providers/ResrvProvider.php
@@ -284,6 +284,13 @@ class ResrvProvider extends AddonServiceProvider
                 ->route('resrv.export.index')
                 ->icon('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="24" width="24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M12 15.75V3.75"/><path d="m7.5 11.25 4.5 4.5 4.5-4.5"/><path d="M21 17.25v3a.75.75 0 0 1-.75.75H3.75A.75.75 0 0 1 3 20.25v-3"/></g></svg>');
 
+            if ($addon = Addon::get('reachweb/statamic-resrv')) {
+                $nav->create(ucfirst(__('Settings')))
+                    ->section('Resrv')
+                    ->can('configure addons')
+                    ->url($addon->settingsUrl())
+                    ->icon('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="24" width="24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><circle cx="12" cy="12" r="3"/><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 1 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 1 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z"/></g></svg>');
+            }
         });
     }
 
